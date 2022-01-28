d60923be10e448b21a836a56c5f3ef6c8156e32d
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2022-01-28 08:44:46 +0700

    update asset & nested widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text("basical")
+          title: Text("basical"),
+          backgroundColor: Colors.teal,
+          actions: [
+            IconButton(icon: Icon(Icons.mail), onPressed: () {}),
+          ]
         ),
         body: SafeArea(child: Text("Basical Flutter"),)
       ),
