227a388f06010d6d8875732b99b7f41bd5291507
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2022-01-31 10:57:53 +0700

    fonts

---
+++ lib/font_style.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+TextStyle mainHeader = TextStyle(
+  fontSize: 26,
+  color: Colors.black,
+  fontFamily: 'Poppins',
+);
+
+TextStyle subHeader = TextStyle(
+  fontSize: 16,
+  color: Colors.black,
+  fontFamily: 'Poppins',
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'font_style.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -16,29 +16,28 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("basical"),
-          backgroundColor: Colors.teal,
-          actions: [
-            IconButton(icon: Icon(Icons.mail), onPressed: () {}),
-          ]
-        ),
-        body: SafeArea(
-          child: Container(
-            color: Colors.white,
-            margin: EdgeInsets.all(10),
-            padding: EdgeInsets.all(10),
-            child: Column(
-              children: [
-                Image(
-                  image: AssetImage("assets/images/bot.jpg"),
-                ),
-                Text("We are bot")
-              ],
+          appBar: AppBar(
+              title: Text("basical"),
+              backgroundColor: Colors.teal,
+              actions: [
+                IconButton(icon: Icon(Icons.mail), onPressed: () {}),
+              ]),
+          body: SafeArea(
+            child: Container(
+              color: Colors.white,
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  Image(
+                    image: AssetImage("assets/images/bot.jpg"),
+                  ),
+                  Text("We are bot", style: mainHeader),
+                  Text("Hey dont worry my friend, ill protect you", style: subHeader),
+                ],
+              ),
             ),
-          ),
-        )
-      ),
+          )),
     );
   }
 }
